398b97590186d35f950defb0a46f7ce1ec2a213b
HuynhManhTam <huynhtam258@gmail.com>
2025-05-24 16:51:37 +0700

    feat: header portfolio

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,49 @@
 <template>
-  <main class="p-2 mt-10">
-    <slot></slot>
-  </main>
+  <div class="container mx-auto max-w-4xl">
+    <header class="flex justify-between items-start md:items-center mt-5">
+      <div class="flex items-center md:space-x-12">
+        <div class="hidden md:block">
+          <NuxtLink class="text-xl font-semibold p-2 hover:bg-gray-200 dark:hover:bg-gray-800">
+            Tam Huynh
+          </NuxtLink>
+        </div>
+
+        <Menu />
+      </div>
+
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
+    </header>
+    
+    <main class="p-2 mt-10">
+      <slot></slot>
+    </main>
+  </div>
 </template>
 
 <script lang="ts" setup>
+useHead({
+  titleTemplate: '%s - Tam Huynh',
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+body {
+  font-family: 'Roboto';
+}
+body {
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  css: ['~/styles/main.scss'],
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
